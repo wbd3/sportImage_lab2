875198dac402a1f254c492eb399ec7b9a0f5a616
wbd3 <willdow@econ1-191-185-dhcp.int.colorado.edu>
2014-09-21 19:10:27 -0600

    SportImage

--- sportImage_lab2/ViewController.swift
+++ sportImage_lab2/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var sportImage: UIImageView!
+    @IBAction func chooseTeam(sender: UIButton) {
+        if sender.tag==1 {
+            sportImage.image=UIImage(named: "rockies_logo.png")
+            messageLabel.text="I'm a Rockies fan too" + "\(nameField.text)"
+        }
+        if sender.tag==2 {
+            sportImage.image=UIImage(named: "broncos_logo.png")
+            messageLabel.text="Get ready for Broncos season" + "\(nameField.text)"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
